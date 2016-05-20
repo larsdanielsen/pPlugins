fe1cd032a6d3b5d4bb65f0bab0abf571df3385e3
Lars Danielsen <lars@pragmasoft.dk>
2016-05-21 00:37:43 +0200

    Themed

--- pPlugins/Scripts/main.js
+++ pPlugins/Scripts/main.js
@@ -4,21 +4,45 @@ $(document).ready(function () {
     var simpleCss = $('link[data-theme=simple]');
     $('body').on('click.styling', 'input[name=theme]', function (e) {
         var theme = $(this).data('theme');
+        setCookie('themeIndex', theme);
         $('body').fadeOut(10, function () {
             if (theme == 'bootstrap') {
-                bootstrapCss.insertAfter('link[data-type=base]');
+                bootstrapCss.insertAfter('[data-type=base]');
                 simpleCss.detach();
             } else if (theme == 'simple') {
-                simpleCss.insertAfter('link[data-type=base]');
+                simpleCss.insertAfter('[data-type=base]');
                 bootstrapCss.detach();
             }
         }).fadeIn();
     });
 
-    setTheme(1);
+    var theme = getCookie('themeIndex') || 'bootstrap';
+    setTheme(theme);
 
     function setTheme(i) {
-        $($('input[name=theme]').get(i)).prop('checked', true).trigger('change.pCheckRadio').trigger('click.styling');
+        $('input[name=theme][data-theme=' + theme + ']').prop('checked', true).trigger('change.pCheckRadio').trigger('click.styling');
     }
    
 });
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires + "; path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
